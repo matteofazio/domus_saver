f9d1f0709d216279586e292876496a8f0564b4b0
matteofazio <115035491+matteofazio@users.noreply.github.com>
2022-10-06 09:22:46 +0200

    Update discordBot.py

--- discordBot.py
+++ discordBot.py
@@ -16,9 +16,14 @@ client = commands.Bot(command_prefix='', intents=discord.Intents.all())
 #ID channels
 
 carta_identità_italia_nuova = 1022173249855029384
-casa = []
-host_capo = []
 
+
+#class variables
+
+host_capo = ['']
+casa_ID = ['']
+date_to = ['']
+date_from = ['']
 
 @client.event
 async def on_connect():
@@ -39,7 +44,51 @@ async def save(ctx):
         with open(imageName,'wb',) as out_file:
             print('Saving image: ' + imageName)
             shutil.copyfileobj(r.raw, out_file)
-            shutil.move(out_file, 'C:\\FAZIO\\Desktop\\QUESTURE')
+            shutil.move(out_file, 'C:\\FAZIO\\Desktop\\QUESTURE',)
+
+
+@client.command(name = 'casa')
+async def casa(ctx):
+	casa_ID = ['']
+	if ctx.message.channel.id==carta_identità_italia_nuova:
+		print(f'Messaggio: {ctx.message.content}')
+	if ctx.message.author == client.user:
+		return
+	casa_ID.append(ctx.message.content[5:])
+	await ctx.message.channel.send(f'Saved info about casa')
+
+
+@client.command(name = 'da')
+async def da(ctx):
+	date_from = ['']
+	if ctx.message.channel.id==carta_identità_italia_nuova:
+		print(f'Messaggio: {ctx.message.content}')
+	if ctx.message.author == client.user:
+		return	
+	date_from.append(ctx.message.content[3:])
+	await ctx.message.channel.send(f'Salvata la data di check in')
+
+
+@client.command(name = 'a')
+async def a(ctx):
+	date_to = ['']
+	if ctx.message.channel.id==carta_identità_italia_nuova:
+		print(f'Messaggio: {ctx.message.content}')
+	if ctx.message.author == client.user:
+		return
+	date_to.append(ctx.message.content[2:])
+	await ctx.message.channel.send(f'Salvata la data di check out')
+
+
+@client.command(name = 'capo')
+async def capo(ctx):
+	host_capo = ['']
+	if ctx.message.channel.id==carta_identità_italia_nuova:
+		print(f'Messaggio: {ctx.message.content}')
+	if ctx.message.author == client.user:
+		return
+	host_capo.append(ctx.message.content[5:])
+	await ctx.message.channel.send(f'Saved info about capo')
 
 
 @client.command(name = 'info')
@@ -48,23 +97,36 @@ async def info(ctx):
 		print(f'Messaggio: {ctx.message.content}')
 	if ctx.message.author == client.user:
 		return
-	if ctx.message.content[5:9]=="casa":
-		casa.append(ctx.message.content)
-		await ctx.message.channel.send(f'Saved info about casa')
-	elif ctx.message.content[5:9]=="capo":
-		host_capo.append(ctx.message.content)
-		await ctx.message.channel.send(f'Saved info about capo')
-	elif ctx.message.content == 'Starting ...' or ctx.message.content == 'Saved!' or ctx.message.content == 'Mi sono connesso ora!':
-		return
-	else:
-		host_capo.append('')
-		await ctx.message.channel.send(f'Messaggio errato! \nSe si vuole salvare la casa digitare: casa ... \nSe si vuole salvare il soggetto come capo digitare: capo ...')
-	print(casa, host_capo)
+	await ctx.message.channel.send(f'Capogruppo: {host_capo[-1]} \nCasa n°{casa_ID[-1]} \nData check-in: {date_from[-1]}\nData check-out:{date_to[-1]}')
+	print(casa, host_capo, date_from, date_to)
 
+@client.command(name = 'ID')
+async def ID(ctx):
+	if ctx.message.channel.id==carta_identità_italia_nuova:
+		print(f'Messaggio: {ctx.message.content}')
+	if ctx.message.author == client.user:
+		return
+	elenco_case = ['Via xxx = 1', 'Via yyy = 2']
+	a = ''
+	for i in elenco_case:
+		a = a + i + '\n'
+	await ctx.message.channel.send(a)
+
+
+@client.command(name = 'comandi')
+async def comandi(ctx):
+	if ctx.message.channel.id==carta_identità_italia_nuova:
+		print(f'Messaggio: {ctx.message.content}')
+	if ctx.message.author == client.user:
+		return
+	elenco_comandi = ['a:   inserisci data check-out','capo:   dichiara se è il capogruppo', 'casa:   inserisci ID casa','da:   inserisci data check-in','info:   controlla i dati inseriti','ID:   richiedi ID case', 'save:   salva l\'immagine']
+	b = ''
+	for j in elenco_comandi:
+		b = b + j + '\n'
+	await ctx.message.channel.send(b)
 
 @client.event
 async def on_ready():
     print("Bot is ready")
-
 
 client.run(os.environ['DISCORD_TOKEN_discord'])
